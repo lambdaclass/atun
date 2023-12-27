5b514ba327db5f0d1868c8c7a1d81848f71de5d6
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-12-27 13:53:54 -0300

    Add bash script skeleton for running client and server

---
+++ scripts/run_client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# sudo setcap cap_net_admin=eip target/release/wontun
+
+target/release/atun --peer 172.18.0.2:19988 &
+
+pid=$!
+
+# FIXME: Check how to do in MacOS
+# sudo ip addr add 10.8.0.3/24 dev tun0
+# sudo ip link set up dev tun0
+# sudo ip link set dev tun0 mtu 1400
+
+
+trap "kill $pid" INT TERM
+
+wait $pid
