ccb931cec93af0f7b45067c994d689d42e82fecf
Pedro Fontana <fontana.pedro93@gmail.com>
2023-12-27 13:40:20 -0300

    Impl VpnDevice

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use std::{
+    io::{self, Read},
     net::{SocketAddrV4, UdpSocket},
     sync::Mutex,
 };
@@ -14,4 +15,24 @@ pub struct VpnDevice {
     /// tun device
     iface: Device,
     peer: Peer,
+}
+
+impl VpnDevice {
+    fn loop_listen_iface(&mut self) -> io::Result<()> {
+        // a large enough buffer, recall the MTU on iface was to be set to 1472
+
+        let mut buf = [0u8; 1504];
+
+        loop {
+            let nbytes = self.iface.read(&mut buf[..])?;
+
+            let peer = &self.peer.endpoint.lock().unwrap();
+
+            if let Some(peer_addr) = peer.as_ref() {
+                self.udp.send_to(&buf[..nbytes], peer_addr)?;
+            } else {
+                eprintln!("..no peer");
+            }
+        }
+    }
 }
