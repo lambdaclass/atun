06295713b24ce2a22b1908fb9ab1654b4bdc9344
MauroFab <maurotoscano2@gmail.com>
2023-12-27 12:51:04 -0300

    ping example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::io::Read;
 
-extern crate tun;
+use ::futures::{StreamExt, SinkExt};
+use tokio::sync::futures;
+use packet::{builder::Builder, icmp, ip, Packet};
+use tun::{self, Configuration, TunPacket};
 
-fn main() {
-    let mut config = tun::Configuration::default();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = Configuration::default();
+
     config
-        .address((10, 0, 0, 1))
+        .address((10, 0, 0, 9))
         .netmask((255, 255, 255, 0))
+        .destination((10, 0, 0, 1))
         .up();
 
     #[cfg(target_os = "linux")]
@@ -14,11 +19,41 @@ fn main() {
         config.packet_information(true);
     });
 
-    let mut dev = tun::create(&config).unwrap();
-    let mut buf = [0; 4096];
+    #[cfg(target_os = "windows")]
+    config.platform(|config| {
+        config.initialize();
+    });
 
-    loop {
-        let amount = dev.read(&mut buf).unwrap();
-        println!("{:?}", &buf[0..amount]);
+    let dev = tun::create_as_async(&config)?;
+
+    let mut framed = dev.into_framed();
+
+    while let Some(packet) = framed.next().await {
+        let pkt = packet?;
+        match ip::Packet::new(pkt.get_bytes()) {
+            Ok(ip::Packet::V4(pkt)) => {
+                if let Ok(icmp) = icmp::Packet::new(pkt.payload()) {
+                    if let Ok(icmp) = icmp.echo() {
+                        println!("{:?} - {:?}", icmp.sequence(), pkt.destination());
+                        let reply = ip::v4::Builder::default()
+                            .id(0x42)?
+                            .ttl(64)?
+                            .source(pkt.destination())?
+                            .destination(pkt.source())?
+                            .icmp()?
+                            .echo()?
+                            .reply()?
+                            .identifier(icmp.identifier())?
+                            .sequence(icmp.sequence())?
+                            .payload(icmp.payload())?
+                            .build()?;
+                        framed.send(TunPacket::new(reply)).await?;
+                    }
+                }
+            }
+            Err(err) => println!("Received an invalid packet: {:?}", err),
+            _ => {}
+        }
     }
+    Ok(())
 }
